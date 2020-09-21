fbdf6455a91d1f8402ac0f3bb39cb787ed6e2c6c
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-09-20 21:40:14 -0300

    Se agrega SplashScreen

--- app/src/main/java/com/example/conversortemperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversortemperatura/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.conversortemperatura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupUI()
     }
+
+    private fun setupUI() {
+
+    }
+
+
+
+
+
+
+    override fun onStart(){
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")    }
 }
